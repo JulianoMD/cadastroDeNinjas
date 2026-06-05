2210110a331d7e43a18bf3f55a368c0c09f7ead8
Juliano Duarte <juliano.mimura.duarte@gmail.com>
2026-06-05 04:25:32 -0300

    Stop tracking .env (add to .gitignore)

---
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.cadastrodeninjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRpository extends JpaRepository<NinjaModel, Long> {
+}
