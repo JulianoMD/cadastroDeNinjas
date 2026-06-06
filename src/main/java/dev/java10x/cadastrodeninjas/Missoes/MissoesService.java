6306e7e5913dcf7f407afa2f9a6d3c22897653f6
Juliano Duarte <juliano.mimura.duarte@gmail.com>
2026-06-05 23:09:33 -0300

    feat: Adicionado metodos de listar missoes por id (todas tambem), adicionar missao, e deletar missao no MissoesService

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesService.java
@@ -1,4 +1,37 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private final MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Adicionar nova missao (POST)
+    public MissoesModel adicionarMissao(MissoesModel missoesModel) {
+        return missoesRepository.save(missoesModel);
+    }
+
+    // Listar todas as missoes (READ)
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    // Listar missao por id (READ)
+    public MissoesModel listarMissaoPorId(Long id) {
+        Optional<MissoesModel> missoesModel = missoesRepository.findById(id);
+        return missoesModel.orElse(null);
+    }
+
+    // Apagar missao (DELETE)
+    public void deletarMissaoPorId(Long id) {
+        missoesRepository.deleteById(id);
+    }
 }
